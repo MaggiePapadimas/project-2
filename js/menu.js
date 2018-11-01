2570a34d072f99e2f03ee0c3e1421094f56c86eb
maggie <maggie.papadimas@hotmail.com>
2018-11-01 14:52:05 -0400

    working on menu

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+//Menu for the game
+
+// will add different version maybe?
+// example :
+  // player vs player
+  // player vs AI
+  // AI vs AI
+function menu (){
+  background(20,150,80);
+  textAlign(CENTER);
+  textSize(35);
+  fill(9);
+  text("PONG", width/2, height/4, 50,50);
+  
+}
+
+menu.prototype.display = function () {
+
+}
