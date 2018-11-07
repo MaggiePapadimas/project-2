e0cd822cfeaaa5e9969dd1f2cd8b45a3fe29a8ff
maggie <maggie.papadimas@hotmail.com>
2018-11-07 09:06:36 -0500

    added pictures for menu buttons

--- js/menu.js
+++ js/menu.js
@@ -14,18 +14,18 @@ var button3
 
 
 function preload(){
-  new loadImage();
+  button1 = loadImage("assets/images/playervsplayer-pixelart.png");
+  button2 = loadImage("assets/images/pixil-frame-0.png");
+  button3 = loadImage("assets/images/ai-vs-ai-pixilart.png");
 }
 
-function menu (){
+menu.prototype.display = function () {
   background(20,150,80);
   textAlign(CENTER);
   textSize(35);
   fill(9);
   text("PONG", width/2, height/4, 50,50);
-
-}
-
-menu.prototype.display = function () {
-
+  button1 = width/4;
+  button2 = width/2;
+  button3 = width/1;
 }
